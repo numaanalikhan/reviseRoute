91661698186c533cedd0a2e59f614b2aed9df437
Bozai Numaan Ali Khan <numaanalikhan99@gmail.com>
2024-10-07 12:45:34 +0530

    routing with url

---
+++ src/Components/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Home (){
+    return(
+        <>
+        <h1>Home Component</h1>
+        </>
+    )
+}
+
+export default Home
+// name default and ??????
---
+++ src/Components/NotFound.js
@@ -0,0 +1,10 @@
+function NotFound (){
+    return(
+        <div style={{textAlign:"center", margin:"10px"}}>
+        <h1>404 Route Not Found</h1>
+        <img style={{marginTop:"5px"}} src="https://th.bing.com/th/id/OIP.jtVcoHDPXhZ_3zFmykSvxgAAAA?rs=1&pid=ImgDetMain" alt="not found"/>
+        </div>
+    )
+}
+
+export default NotFound;
